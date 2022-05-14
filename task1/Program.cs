f141b790d7ebb399bac0ea5c1389187b491ebbcd
Nikolay_Davydov <skmoscow74@yandex.ru>
2022-05-14 13:32:54 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int secondDigit(int a){
+    int result = a/10;
+    result = result%10;
+    return result;
+}
+int x = 0;
+while (true){
+ Console.WriteLine("Введите трехзначное число: ");
+ x= int.Parse(Console.ReadLine());
+    if (x > 999 || x < 100) Console.WriteLine ("Введенное число не является трехзначным.");
+    else break;
+}
+ Console.WriteLine(secondDigit(x));
