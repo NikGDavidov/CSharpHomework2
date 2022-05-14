fd3d8b6d9511c15f65808735b66dcec0a1b49d11
Nikolay_Davydov <skmoscow74@yandex.ru>
2022-05-14 22:57:34 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает, что третьей цифры
+// нет.
+
+int numberOfDigits(int a){
+    int i = 0;
+    while (a>0){
+        a = a/10;
+        i++;
+    }return i;
+}
+Console.WriteLine("Введите любое целое число");
+ int x= int.Parse(Console.ReadLine());
+ if (x<0) x=-x;
+ int i = numberOfDigits(x);
+ if (i>2){
+     int delitel = (int) Math.Pow (10, i-3);
+     int result = x/delitel;
+     result = result%10;
+     Console.WriteLine(result);
+ }else Console.WriteLine("Третьей цифры нет.");
